9dcdf4440a140a99ae3a20107d15bf34d04a703b
jakob <jakobyaro99@gmail.com>
2021-04-09 17:48:27 +0200

    test for IndexController

--- spring5-recipe-app-main/src/main/java/guru/springframework/controllers/IndexController.java
+++ spring5-recipe-app-main/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,9 @@
 package guru.springframework.controllers;
 
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.service.RecipeService;
+import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
@@ -17,7 +13,6 @@ public class IndexController {
     public IndexController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
-
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
